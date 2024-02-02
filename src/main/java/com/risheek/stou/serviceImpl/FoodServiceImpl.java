7f020017509af483ac1b2ae537bb72b90b67de46
Risheek Narayanadevarakere <risheeks@gmail.com>
2024-02-02 04:32:38 -0600

    food endpoints impl

---
+++ src/main/java/com/risheek/stou/serviceImpl/FoodServiceImpl.java
@@ -0,0 +1,63 @@
+package com.risheek.stou.serviceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.risheek.stou.model.Food;
+import com.risheek.stou.repository.FoodRepository;
+import com.risheek.stou.service.FoodService;
+
+@Service
+public class FoodServiceImpl implements FoodService {
+
+	private final FoodRepository foodRepository;
+	
+	public FoodServiceImpl(FoodRepository foodRepository) {
+		super();
+		this.foodRepository = foodRepository;
+	}
+
+	@Override
+	public List<Food> getAllFoods() {
+		return this.foodRepository.findAll();
+	}
+
+	@Override
+	public Optional<Food> getFoodById(Integer foodId) {
+		if(this.foodRepository.existsById(foodId)) {
+			return this.foodRepository.findById(foodId);
+		}
+		return null;
+	}
+
+	@Override
+	public List<Food> getFoodsByCook(String cookEmail) {
+		return this.foodRepository.getFoodByCookEmail(cookEmail);
+	}
+
+	@Override
+	public Food createFood(Food food) {
+		food.setFoodId(food.getFoodId());
+		System.out.println(food);
+		return this.foodRepository.save(food);
+	}
+
+	@Override
+	public Food updateFood(Food food) {
+		if(this.foodRepository.existsById(food.getFoodId()))
+			return this.foodRepository.save(food);
+		return null;
+	}
+
+	@Override
+	public boolean deleteFood(Integer foodId) {
+		if(this.foodRepository.existsById(foodId)) {
+			this.foodRepository.deleteById(foodId);
+			return true;
+		}
+		return false;
+	}
+
+}
